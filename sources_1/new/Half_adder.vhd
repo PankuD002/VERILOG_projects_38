9f846cdb9237995ec253a37987d8b340c7970e2f
Pankaj Dewhare <96539244+PankuD002@users.noreply.github.com>
2024-07-14 13:04:41 +0530

    Add files via upload

---
+++ sources_1/new/Half_adder.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 14.07.2024 12:06:03
+-- Design Name: 
+-- Module Name: Half_adder - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Half_adder is
+    Port ( a : in STD_LOGIC;
+           b : in STD_LOGIC;
+           sum : out STD_LOGIC;
+           carry : out STD_LOGIC);
+end Half_adder;
+
+architecture Behavioral of Half_adder is
+
+begin
+    sum<= a XOR b;
+    carry <= a AND b;
+
+
+end Behavioral;
